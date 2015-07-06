d219a1b8a31af4886522fb4ef764b405773ddd04
Christopher Nguyen <christopher.nguyen.huy@gmail.com>
2015-07-06 11:47:32 -0400

    updated with string buffer

--- snippets/stringconcats.py
+++ snippets/stringconcats.py
@@ -1,22 +1,40 @@
-### Stringbuffer Method ###
+import time
+try:
+	from cStringIO import StringIO
+except:
+	from io import StringIO
 
-from cStringIO import StringIO
-# python3:  from io import StringIO
+# 10x more, aka 10000000, += will take 200 seconds, be carefull!
+qty = 1000000
 
+start = time.time()
+li = []
+for j in range(0,qty):
+	li.append(str(j))
+# print(''.join(li))
+somestring = ''.join(li)
+end = time.time()
+elapsed = end - start
+print('List join time: %s' % elapsed)
+
+somestring = ''
 buf = StringIO()
+start = time.time()
+for j in range(0,qty):
+	buf.write(str(j)) 
+# print(somestring)
+somestring = buf.getvalue()
+end = time.time()
+elapsed = end - start
+print('String IO time: %s' % elapsed)
 
-buf.write('foo')
-buf.write('foo')
-buf.write('foo')
 
-buf.getvalue()
-# 'foofoofoo'
-
-### List Method ###
-
-l = []
-l.append('foo')
-l.append('bar')
-l.append('baz')
-
-s = ''.join(l)
+### Super slow!!! ###
+somestring = ''
+start = time.time()
+for j in range(0,qty):
+	somestring += str(j)
+# print(somestring)
+end = time.time()
+elapsed = end - start
+print('+= time: %s' % elapsed)
