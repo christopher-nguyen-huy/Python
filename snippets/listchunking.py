ca578f614bee6a59a1e3a5f4621c1ea9183a3fc7
Christopher Nguyen <christopher.nguyen.huy@gmail.com>
2016-02-17 21:23:23 -0500

    added list dechunking

--- snippets/listchunking.py
+++ snippets/listchunking.py
@@ -1,2 +1,12 @@
 # Splits array into blocks of 100
 chunks=[data[x:x+100] for x in xrange(0, len(data), 100)]
+
+
+# Dechunking
+r = [[1,2,3],[4,5,6], [7], [8,9]]
+s = [item for sublist in r for item in sublist]
+
+# Is equivalent to this!
+for sublist in r:
+	for item in sublist:
+		s.append(item)
