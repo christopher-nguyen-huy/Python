56d7b4e578803737e2a8b5fd5bc2f2e697726f8d
Christopher Nguyen <christopher.nguyen.huy@gmail.com>
2016-03-19 13:43:43 -0400

    multiple string

---
+++ snippets/checkmultstringinstring.py
@@ -0,0 +1,8 @@
+# check if multiple strings exists in another string
+
+# You can use any:
+
+if any(x in str for x in a):
+	pass
+
+# Similarly to check if all the strings from the list are found, use all instead of any.
